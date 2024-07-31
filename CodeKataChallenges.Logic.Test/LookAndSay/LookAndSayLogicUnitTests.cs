29acd99bb8baf6a230357f6e8e57f7e74741c44b
Jack Read <jackpendarvesread@gmail.com>
2024-07-31 22:01:14 +0100

    LookAndSay initial implementation

---
+++ CodeKataChallenges.Logic.Test/LookAndSay/LookAndSayLogicUnitTests.cs
@@ -0,0 +1,32 @@
+﻿using CodeKataChallenges.Logic.LookAndSay;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKataChallenges.Logic.Test.LookAndSay
+{
+    public class LookAndSayLogicUnitTests
+    {
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(2, "11")]
+        [InlineData(3, "21")]
+        [InlineData(4, "1211")]
+        [InlineData(5, "111221")]
+        [InlineData(6, "312211")]
+        [InlineData(7, "13112221")]
+        [InlineData(8, "1113213211")]
+        [InlineData(9, "31131211131221")]
+        [InlineData(10, "13211311123113112211")]
+        public void Test1(int n, string expected)
+        {
+            var underTest = new LookAndSayLogic();
+            var result = underTest.GetNthTerm(n);
+            result.Should().Be(expected);
+        }
+
+    }
+}
---
+++ CodeKataChallenges.Logic/LookAndSay/LookAndSayLogic.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKataChallenges.Logic.LookAndSay
+{
+    public class LookAndSayLogic
+    {
+        public string GetNthTerm(int n)
+        {
+            if(n <= 0)
+            {
+                return "";
+            }
+
+            string s = "1";
+            for(int i = 1; i < n; i++)
+            {
+                s = GetNextTerm(s);
+            }
+
+            return s;
+        }
+
+        private string GetNextTerm(string prev)
+        {
+            var sb = new StringBuilder();
+            int count = 0;
+            char current = 'x';
+            foreach(char c in prev)
+            {
+                if(c != current)
+                {
+                    if(count > 0)
+                    {
+                        sb.Append(count);
+                        sb.Append(current);
+                    }
+
+                    current = c;
+                    count = 0;
+                }
+                count++;
+            }
+
+            if(count > 0)
+            {
+                sb.Append(count);
+                sb.Append(current);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
