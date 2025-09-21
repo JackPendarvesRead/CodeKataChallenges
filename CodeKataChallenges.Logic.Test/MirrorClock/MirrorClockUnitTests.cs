e8a3d273bad7bd2ccf27a61a3d13db0860724f37
JackRead <jackpendarvesread@gmail.com>
2025-09-21 14:25:53 +0100

    MirroClock initial implementation

---
+++ CodeKataChallenges.Logic.Test/MirrorClock/MirrorClockUnitTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKataChallenges.Logic.Test.MirrorClock
+{
+    public class MirrorClockUnitTests
+    {
+        [Theory]
+        [InlineData("12:00", "12:00")]
+        [InlineData("01:00", "11:00")]
+        [InlineData("06:00", "06:00")]
+        [InlineData("11:00", "01:00")]
+        [InlineData("10:15", "01:45")]
+        [InlineData("05:25", "06:35")]
+        [InlineData("01:50", "10:10")]
+        [InlineData("11:58", "12:02")]
+        [InlineData("12:01", "11:59")]
+        public void WhatIsTheTime_ShouldReturnCorrectTime_WhenGivenMirrorTime(string mirrorTime, string expectedTime)
+        {
+            // Arrange
+            var mirrorClock = new Logic.MirrorClock.MirrorClock();
+            // Act
+            var result = mirrorClock.WhatIsTheTime(mirrorTime);
+            // Assert
+            Assert.Equal(expectedTime, result);
+        }
+
+        [Theory]
+        [InlineData("13:00")]
+        [InlineData("00:30")]
+        [InlineData("10:60")]
+        [InlineData("10:5")]
+        [InlineData("9:30")]
+        [InlineData("abc")]
+        [InlineData("12-30")]
+        [InlineData("1230")]
+        [InlineData("")]
+        [InlineData(null)]
+        public void WhatIsTheTime_ShouldThrowArgumentException_WhenGivenInvalidTimeFormat(string input)
+        {
+            // Arrange
+            var mirrorClock = new Logic.MirrorClock.MirrorClock();
+            Assert.Throws<ArgumentException>(() => mirrorClock.WhatIsTheTime(input));
+        }
+    }
+}
---
+++ CodeKataChallenges.Logic/MirrorClock/MirrorClock.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CodeKataChallenges.Logic.MirrorClock
+{
+    public class MirrorClock
+    {
+        private static Regex inputValidationRegex = new Regex(@"^(0[1-9]|1[0-2]):([0-5][0-9])$");
+
+        public string WhatIsTheTime(string timeInMirror)
+        {
+            if(string.IsNullOrWhiteSpace(timeInMirror))
+            {
+                throw new ArgumentException("Input time cannot be null or empty");
+            }
+
+            if (!inputValidationRegex.IsMatch(timeInMirror))
+            {
+                throw new ArgumentException("Input time is not in the correct format (hh:mm) where hh is between 01-12 and mm is between 00-59");
+            }
+
+            var timeParts = timeInMirror.Split(':');
+            var hours = int.Parse(timeParts[0]);
+            var minutes = int.Parse(timeParts[1]);
+
+            minutes = minutes == 0  ? 0 : 60 - minutes;
+            hours = 12 - hours - (minutes == 0 ? 0 : 1);
+            if(hours < 1)
+            {
+                hours += 12;
+            }
+
+            return $"{hours:D2}:{minutes:D2}";
+        }
+    }
+}
